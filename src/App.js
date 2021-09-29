044fd5682e2b898a19fa647d8ba535f109eb63c3
(kondo tatsuya) <(tacchann@i.softbank.jp)>
2021-09-29 18:30:01 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Title from './components/Title'
+import Navigation from './components/Navigation';
+import Field from './components/Field';
+import Button from './components/Button';
+import ManipulationPanel from './components/ManipulationPanel';
+import useSnakeGame from './hooks/useSnakeGame'
 
 function App() {
+  const {
+    body,
+    difficulty,
+    fields,
+    start,
+    stop,
+    reload,
+    status,
+    updateDirection,
+    updateDifficulty,
+  } = useSnakeGame()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <Title />
+        <Navigation
+          length={body.length}
+          difficulty={difficulty}
+          onChangeDifficulty={updateDifficulty}
+        />
       </header>
+      <main className="main">
+        <Field fields={fields} />
+      </main>
+      <footer className="footer">
+        <Button
+          status={status}
+          onStop={stop}
+          onStart={start}
+          onRestart={reload}
+        />
+        <ManipulationPanel onChange={updateDirection} />
+      </footer>
     </div>
   );
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <div className="title-container">
+            <h1 className="title">Snake Game</h1>
+        </div>
+    );
+};
+
+export default Title;
